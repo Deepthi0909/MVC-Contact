c21c90e49d329bbf265efe3692b49e260456ab15
Deepthi0909 <57625801+Deepthi0909@users.noreply.github.com>
2019-11-18 18:37:33 +0530

    Add files via upload

---
+++ Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactMvcApplication.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public DateTime BirthDate { get; set; }
+        public long MobileNo { get; set; }
+        public string WorkPhone { get; set; }
+        public string HomePhone { get; set; }
+
+
+    }
+}
